2a943ce3ee18efc158a144fcba70dd8fa2de6a9f
aastharasekar <aastharasekar3@gmail.com>
2025-07-15 01:20:53 +0530

    Q3

---
+++ Q2.java
@@ -0,0 +1,23 @@
+class Q2{
+    public static void main(String[] args) {
+        
+        char remarks = 'O';
+
+        switch (remarks){
+            
+            case 'O': System.out.println("Outstanding");
+            break;
+
+            case 'A':System.out.println("Excellent");
+            break;
+
+            case 'B':System.out.println("Average");
+            break;
+            case 'F':System.out.println("Fail");
+            break;
+
+            default:System.out.println("No such grade present");
+            break;
+        }
+    }
+}
