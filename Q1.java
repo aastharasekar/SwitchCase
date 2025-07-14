31802415598eeaad2e5f0dcd045538925cd24543
aastharasekar <aastharasekar3@gmail.com>
2025-07-15 01:12:22 +0530

    q1

---
+++ Q1.java
@@ -0,0 +1,20 @@
+class SwitchCase{
+    public static void main(String[] args) {
+        
+        String color = "Blue";
+
+        switch (color){
+            case "Red":System.out.println("STOP");
+            break;
+
+            case "Yellow":System.out.println("Ready");
+            break;
+
+            case "Green":System.out.println("Go");
+            break;
+
+            default:System.out.println("No such color exists in Traffic lights");
+            break;
+        }
+    }
+}
